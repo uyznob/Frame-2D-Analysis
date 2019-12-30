ed2439ffb5c501d1909815022a693f14dec5e5aa
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2019-12-30 17:17:14 +0700

    Initial

---
+++ app/src/main/java/com/j97/app/MainActivity.java
@@ -0,0 +1,19 @@
+package com.j97.app;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+    }
+
+}
