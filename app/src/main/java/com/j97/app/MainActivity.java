729e701dc144c23f8583b94a38e3a93aa370735a
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2019-12-30 19:24:36 +0700

    first commit (#1)

--- app/src/main/java/com/j97/app/MainActivity.java
+++ app/src/main/java/com/j97/app/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-
     }
 
 }
